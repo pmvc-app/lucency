0577bf470d3b9f09adcf7ee693759228e9295205
Hill <hill@kimo.com>
2017-01-08 21:13:09 +0000

    fixed unit test

--- tests/test.php
+++ tests/test.php
@@ -34,8 +34,7 @@ class LucyTest extends PHPUnit_Framework_TestCase
         $c->plugApp(['../']);
         $result = $c->process();
         $actual = \PMVC\value($result,[0])->get('text');
-
-        $this->assertContains('world', $actual);
+        $this->assertNull($actual);
     }
 }
 
